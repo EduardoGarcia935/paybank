00bddfb2a31085b07ffd11d387bd99e6cd63a945
EduardoGarcia935 <eduardo.agarcia117@gmail.com>
2023-07-26 18:53:31 -0300

    feat: yourSafe.vue

---
+++ src/components/YourSafe.vue
@@ -0,0 +1,137 @@
+<script>
+  export default {
+    name: "YourSafe",
+    components: {},
+  };
+</script>
+
+<template>
+  <section class="container" id="section-5">
+    <div class="row">
+      <div class="grid-5">
+        <h6 class="uppercase color-gradient h7">investimentos</h6>
+        <h3 class="mt-1">
+          A sua segurança é nossa
+          <span class="color-gradient py-1">prioridade</span>
+        </h3>
+        <p class="mt-3">
+          Sua segurança, confiabilidade, controle de suas finanças é o principal
+          compromisso da Paybank.
+        </p>
+
+        <div class="mt-3 pb-4 bb-gray hidden flex-start-row">
+          <div class="bg-icon-white icon-click mr-1">
+            <img
+              src="../assets/img/icon-smartphone.svg"
+              class="icon"
+              alt=""
+              onload="SVGInject(this)"
+            />
+          </div>
+
+          <div class="bg-icon-white icon-click mr-1">
+            <img
+              src="../assets/img/icon-fingerprint.svg"
+              class="icon"
+              alt=""
+              onload="SVGInject(this)"
+            />
+          </div>
+
+          <div class="bg-icon-white icon-click mr-1">
+            <img
+              src="../assets/img/icon-card.svg"
+              class="icon"
+              alt=""
+              onload="SVGInject(this)"
+            />
+          </div>
+
+          <div class="bg-icon-white icon-click mr-1">
+            <img
+              src="../assets/img/icon-shield.svg"
+              class="icon"
+              alt=""
+              onload="SVGInject(this)"
+            />
+          </div>
+        </div>
+
+        <div class="mt-3 relative" style="min-height: 180px">
+          <div class="text-click">
+            <h4>Controle do cartão pelo App</h4>
+            <p class="mt-2">
+              Perdeu o cartão ou suspeita de roubo? Bloqueie e desbloqueie o
+              cartão pelo aplicativo em segundos.
+            </p>
+          </div>
+
+          <div class="text-click">
+            <h4>Acesso protegido</h4>
+            <p class="mt-2">
+              Uma camada extra de segurança para acessar o aplicativo com
+              reconhecimento facial, impressão digital ou senha.
+            </p>
+          </div>
+
+          <div class="text-click">
+            <h4>Suporte</h4>
+            <p class="mt-2">
+              Digite sua senha para confirmar transações e reporte qualquer
+              movimentação não autorizada por você.
+            </p>
+          </div>
+
+          <div class="text-click">
+            <h4>Transações seguras</h4>
+            <p class="mt-2">
+              Digite sua senha para confirmar transações e reporte qualquer
+              movimentação não autorizada por você.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="grid-1 disappear"></div>
+      <div class="grid-6 relative" style="min-height: 260px">
+        <img
+          src="../assets/img/fig5-paybank.png"
+          class="img-paybank"
+          alt=""
+          loading="lazy"
+          onload="SVGInject(this)"
+        />
+        <img
+          src="../assets/img/fig6-paybank.png"
+          class="img-paybank"
+          alt=""
+          loading="lazy"
+          onload="SVGInject(this)"
+        />
+        <img
+          src="../assets/img/fig7-paybank.png"
+          class="img-paybank"
+          alt=""
+          loading="lazy"
+          onload="SVGInject(this)"
+        />
+        <img
+          src="../assets/img/fig8-paybank.png"
+          class="img-paybank"
+          alt=""
+          loading="lazy"
+          onload="SVGInject(this)"
+        />
+
+        <img
+          src="../assets/img/pattern.svg"
+          class="pattern-paybank"
+          alt=""
+          loading="lazy"
+          onload="SVGInject(this)"
+        />
+      </div>
+    </div>
+  </section>
+</template>
+
+<style scoped></style>
