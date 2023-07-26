dca2017c5c0be913f665677c072c4647380f28da
EduardoGarcia935 <eduardo.agarcia117@gmail.com>
2023-07-26 18:16:38 -0300

    feat: screen loader

---
+++ src/components/ScreenLoader.vue
@@ -0,0 +1,25 @@
+<script>
+  export default {
+    name: "ScreenLoader",
+    components: {},
+  };
+</script>
+
+<template>
+  <div class="screen-loading flex-column">
+    <div class="logo-paybank">
+      <div class="logo-part-1"></div>
+      <div class="logo-part-2"></div>
+    </div>
+    <div class="bar-loader mt-1"></div>
+
+    <img
+      src="../assets/img/bg-hero.svg"
+      class="svg-screen-loading"
+      alt=""
+      onload="SVGInject(this)"
+    />
+  </div>
+</template>
+
+<style scoped></style>
