b5f06aa7f798725edbec425370069eb767089c05
EduardoGarcia935 <eduardo.agarcia117@gmail.com>
2023-07-26 19:00:00 -0300

    feat: future section

---
+++ src/components/Futuro.vue
@@ -0,0 +1,122 @@
+<script>
+  export default {
+    name: "Futuro",
+    components: {},
+  };
+</script>
+
+<template>
+  <section class="container" id="section-3">
+    <div class="row">
+      <div class="grid-6">
+        <h6 class="uppercase color-gradient h7">o futuro</h6>
+        <h2 class="mt-1">
+          É a onda do <span class="color-gradient">futuro</span>
+        </h2>
+
+        <div class="flex-start-row mt-4">
+          <a href="#" class="btn-white flex-start-row">
+            <img
+              src="../assets/img/icon-apple.svg"
+              class="icon fill-svg-green"
+              alt=""
+              onload="SVGInject(this)"
+            />
+            <h6 class="h7 ml-1">Apple&nbsp;Pay</h6>
+          </a>
+          <a href="#" class="btn-white flex-start-row ml-2">
+            <img
+              src="../assets/img/icon-googleplay.svg"
+              class="icon fill-svg-green"
+              alt=""
+              onload="SVGInject(this)"
+            />
+            <h6 class="h7 ml-1">Google&nbsp;Pay</h6>
+          </a>
+        </div>
+        <p class="mt-3">
+          Lorem ipsum dolor sit amet consectetur. Et ut tempus facilisis
+          tristique. Vitae consequat vel tortor aenean imperdiet posuere neque.
+          Sollicitudin at at volutpat scelerisque ut aliquet velit tempor.
+        </p>
+
+        <div class="flex-start-row mt-4 mb-5">
+          <div class="bg-icon">
+            <img
+              src="../assets/img/icon-arrow-up.svg"
+              class="icon"
+              alt=""
+              onload="SVGInject(this)"
+            />
+          </div>
+          <div class="ml-2">
+            <h6>Warranty documents</h6>
+            <p>An obligation in which a third party agrees to repay a debt.</p>
+          </div>
+        </div>
+
+        <div class="flex-start-row mt-2 mb-5">
+          <div class="bg-icon">
+            <img
+              src="../assets/img/icon-card.svg"
+              class="icon"
+              alt=""
+              onload="SVGInject(this)"
+            />
+          </div>
+          <div class="ml-2">
+            <h6>Quick credit</h6>
+            <p>An obligation in which a third party agrees to repay a debt.</p>
+          </div>
+        </div>
+
+        <div class="flex-start-row mt-3">
+          <div class="bg-icon">
+            <img
+              src="../assets/img/icon-download.svg"
+              class="icon"
+              alt=""
+              onload="SVGInject(this)"
+            />
+          </div>
+          <div class="ml-2">
+            <h6>Create Deposit</h6>
+            <p>An obligation in which a third party agrees to repay a debt.</p>
+          </div>
+        </div>
+      </div>
+      <div class="grid-6 relative flex-center px-5">
+        <img src="../assets/img/fig9-paybank.svg" alt="" onload="SVGInject(this)" />
+
+        <div class="bg-icon-white" id="icon1-fig9">
+          <img
+            src="../assets/img/icon-smartphone.svg"
+            class="icon"
+            alt=""
+            onload="SVGInject(this)"
+          />
+        </div>
+
+        <div class="bg-icon-white" id="icon2-fig9">
+          <img
+            src="../assets/img/icon-heart.svg"
+            class="icon"
+            alt=""
+            onload="SVGInject(this)"
+          />
+        </div>
+
+        <div class="bg-icon-white" id="icon3-fig9">
+          <img
+            src="../assets/img/icon-card.svg"
+            class="icon"
+            alt=""
+            onload="SVGInject(this)"
+          />
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<style scoped></style>
